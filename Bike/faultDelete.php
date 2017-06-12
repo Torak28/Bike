4d870040515ccac081be4da2ac2e8e571b258455
Jarosalaw Ciolek-Zelechowski <zelechowski28@gmail.com>
2017-06-12 19:12:58 +0200

    Czyszczenie po Forku

--- Bike/faultDelete.php
+++
@@ -1,9 +0,0 @@
-<?php
-include 'connect.php';
-        $id=$_POST['fault'];
-        if( $mysqli->query("DELETE FROM awarie WHERE id_awarii=$id") ) {
-            echo "Usunieto";
-        }else{
-            echo "Coś nie tak.";
-        }
-?>
--- Bike/ordersCounter.php
+++
@@ -1,8 +0,0 @@
-<?php
-include 'connect.php';
-$id=$_POST['servis'];
-        if($data = $mysqli->query("SELECT COUNT(id_awarii) FROM przypisania_awarii WHERE id_serwis=$id")) {
-            $row = $data->fetch_row();
-            echo $row[0]; 
-        }
-?>
